f84462811ba5309dbbecd3a2ec010633a736dd40
Karolis <karolis.jocevicius@seb.se>
2018-12-20 16:56:53 +0200

    seb bootstrap  added
    seb bootstrap  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbAlertModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
