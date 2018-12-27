640d44bd6e35851b08ef7acef765c14981ac730a
Saulius Paukštys <saulius.p@Sauliuss-MacBook-Pro.local>
2018-12-27 16:32:33 +0200

    tests updated

--- src/app/components/icons/icons.component.spec.ts
+++ src/app/components/icons/icons.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconsComponent } from './icons.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IconsService } from 'src/app/services/icons.service';
+
+const iconsServiceMock = {
+  icons: [],
+};
 
 describe('IconsComponent', () => {
   let component: IconsComponent;
@@ -8,10 +15,12 @@ describe('IconsComponent', () => {
 
   beforeEach((done) => {
     TestBed.configureTestingModule({
-      declarations: [ IconsComponent ]
+      declarations: [IconsComponent],
+      imports: [FormsModule, RouterTestingModule.withRoutes([])],
+      providers: [ { provide: IconsService, useValue: iconsServiceMock } ],
     })
-    .compileComponents()
-    .then(done);
+      .compileComponents()
+      .then(done);
   });
 
   beforeEach(() => {
