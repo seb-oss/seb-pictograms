10e20438204cd2711c19ab426606f6e6fa08df51
Saulius Paukštys <saulius.p@Sauliuss-MacBook-Pro.local>
2018-12-27 16:32:33 +0200

    tests updated

--- src/app/components/icon-preview/icon-preview.component.spec.ts
+++ src/app/components/icon-preview/icon-preview.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IconPreviewComponent } from './icon-preview.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DocumentService } from 'src/app/services/document.service';
+
+const docServiceMock = {
+
+};
 
 describe('IconPreviewComponent', () => {
   let component: IconPreviewComponent;
@@ -7,7 +14,9 @@ describe('IconPreviewComponent', () => {
 
   beforeEach((done) => {
     TestBed.configureTestingModule({
-      declarations: [IconPreviewComponent]
+      declarations: [IconPreviewComponent],
+      imports: [FormsModule, RouterTestingModule.withRoutes([])],
+      providers: [ { provide: DocumentService, useValue: docServiceMock } ],
     })
       .compileComponents()
       .then(done);
--- src/app/services/icons.service.spec.ts
+++ src/app/services/icons.service.spec.ts
@@ -4,9 +4,17 @@ import { IconsService } from './icons.service';
 
 describe('IconsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
+  let service: IconsService;
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      providers: [IconsService],
+    });
+    service = TestBed.get(IconsService);
+  });
 
   it('should be created', () => {
-    const service: IconsService = TestBed.get(IconsService);
+
     expect(service).toBeTruthy();
   });
 });
