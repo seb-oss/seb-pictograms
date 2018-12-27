ca42107bd8e7ea85367514f493361b6f93380178
Gerardas Dilda <gerardas.dilda@seb.se>
2018-12-27 11:14:26 +0200

    Fix

--- src/app/components/icon-preview/icon-preview.component.ts
+++ src/app/components/icon-preview/icon-preview.component.ts
@@ -12,13 +12,13 @@ export class IconPreviewComponent implements OnInit {
   icon: string;
 
   readonly iconSizes = ['seb-size-5x', 'seb-size-3x', 'seb-size-2x', 'seb-size-lg', 'seb-size-sm', 'seb-size-xs'];
-  readonly fontWeights = ['seb-icon', 'seb-light-icon'];
+  readonly fontWeights = [{ type: 'seb-icon', name: 'Normal' }, { type: 'seb-light-icon', name: 'Light' }];
 
   fontWeight: string;
 
   constructor() { }
 
   ngOnInit() {
-    this.fontWeight = this.fontWeights[0];
+    this.fontWeight = this.fontWeights[0].type;
   }
 }
