f01813e5b7534a4620bec1e7d437740384d0ae0b
Saulius Paukštys <saulius.p@Sauliuss-MacBook-Pro.local>
2018-12-27 12:26:26 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IconsComponent } from './components/icons/icons.component';
+import { IconPreviewComponent } from './components/icon-preview/icon-preview.component';
+import { IconsRouteGuard } from './gruards/icons-route.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: IconsComponent },
+  { path: ':icon', component: IconPreviewComponent, canActivate: [IconsRouteGuard] },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
